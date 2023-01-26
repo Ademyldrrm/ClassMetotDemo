b5d4bb1431872fc6b562e2f52b6cc07fd27444a4
AdemYildirim <AdemYildirim@DESKTOP-40G2DNI>
2023-01-26 17:06:31 +0300

    Add project files.

---
+++ CustomerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class CustomerManager
+    {
+        public void Ekle(Customer customer)
+        {
+            Console.WriteLine("Müşteri Eklendi : "+customer.LastName);
+        }
+
+        public void Silme(Customer customer)
+        {
+            Console.WriteLine("Müşteri Silindi : " + customer.FirstName);
+        }
+
+        public void Listele(Customer customer)
+        {
+            Console.WriteLine("Id listelendi : " + customer.Id);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.FirstName = "Adem";
+            customer1.LastName = "Yıldırım";
+            customer1.Id = 1;
+
+            Customer customer2= new Customer();
+            customer2.FirstName = "Ahmet";
+            customer2.LastName = "Yılmaz";
+            customer2.Id = 2;
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Ekle(customer1);
+            customerManager.Ekle(customer2);
+            customerManager.Silme(customer1);
+            customerManager.Silme(customer2);
+            customerManager.Listele(customer1);
+            customerManager.Listele(customer2);
+
+
+        }
+    }
+}
